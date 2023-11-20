f1aecfccd6fd29074ffadaf0b597b3a77cdc39d3
Yazeed <yazoid1421@gmail.com>
2023-11-20 23:47:39 +0300

    faster

--- index.js
+++ index.js
@@ -8,19 +8,41 @@ const multer = require('multer'); // Import multer
 const storage = multer.memoryStorage(); // Store the file in memory
 const upload = multer({ storage: storage });
 
+let browser; // Browser instance
+let pagePool = []; // Pool of pages
+
+async function createBrowser() {
+    browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox'],
+    });
+}
+
+async function createPage() {
+    if (pagePool.length > 0) {
+        return pagePool.pop();
+    } else {
+        const page = await browser.newPage();
+        return page;
+    }
+}
+
+async function releasePage(page) {
+    await page.setContent('');
+    pagePool.push(page);
+}
+
 app.post('/pdf', upload.single('htmlFile'), async (req, res) => { // use `upload.single('htmlFile')` middleware
     try {
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox'],
-        });
-        const page = await browser.newPage();
-
         // Ensure the file was uploaded
         if (!req.file || !req.file.buffer) {
             throw new Error("No file uploaded");
         }
 
         const htmlContent = req.file.buffer.toString('utf8'); // Convert buffer to string
+
+        // Create a new page or reuse an existing one
+        const page = await createPage();
 
         // Set content of the page to the uploaded HTML
         await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
@@ -31,7 +53,8 @@ app.post('/pdf', upload.single('htmlFile'), async (req, res) => { // use `upload
             format: 'A4',
         });
 
-        await browser.close();
+        // Release the page back to the pool
+        await releasePage(page);
 
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'attachment; filename=result.pdf');
@@ -43,6 +66,7 @@ app.post('/pdf', upload.single('htmlFile'), async (req, res) => { // use `upload
     }
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Example app listening on port ${port}`);
+    await createBrowser();
 });
